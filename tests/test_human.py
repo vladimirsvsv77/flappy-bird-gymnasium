6157e2cdcd879a8c3bab430cb155ec1dc4b4499f
Martin Kubovcik <markub3327@gmail.com>
2023-10-27 19:33:20 +0200

    fix

--- tests/test_human.py
+++ tests/test_human.py
@@ -37,7 +37,9 @@ import flappy_bird_gymnasium
 
 
 def play():
-    env = gymnasium.make("FlappyBird-v0", audio_on=True, render_mode="human")
+    env = gymnasium.make(
+        "FlappyBird-v0", audio_on=True, render_mode="human", use_lidar=True
+    )
 
     score, steps = 0, 0
     video_buffer = []
